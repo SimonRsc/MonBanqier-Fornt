2a8b1f7990b1f01551ba34c91bcca0cd7f27971a
SimonRsc <simon.raschetti@grenoble-inp.org>
2020-04-27 16:52:13 +0200

    Erreur chemin server

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/mon-banquier'));
+app.use(express.static(__dirname + '/dist/monBanquier'));
 
 app.get('/*', function(req,res) {
 
-    res.sendFile(path.join(__dirname+'/dist/mon-banquier/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/monBanquier/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
